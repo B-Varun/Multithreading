a67dac3bf2b73f84470ca45ad54b080f05307ab2
B-Varun <bvarun662@gmail.com>
2023-07-18 16:45:55 -0700

    Sample Program1 on Threads

---
+++ DefineThread/Thread_Class/Program1/MyThread_Demo.java
@@ -0,0 +1,10 @@
+package edu.java.threads.demo;
+import edu.java.threads.MyThread;
+public class MyThread_Demo{
+public static void main(String... threads){
+MyThread thread = new MyThread();
+thread.start();
+for(int i=0;i<10;i++)
+ System.out.println("Main Thread");
+}
+}
