dedcecd0ea9c3355a7f2fbd807e68259edcdeb03
B-Varun <bvarun662@gmail.com>
2023-08-05 23:55:01 -0700

    Minor changes to Main class

--- DefineThread/Runnable/Program2/Main.java
+++ DefineThread/Runnable/Program2/Main.java
@@ -4,7 +4,8 @@ import com.thread.Child;
 public class Main{
 public static void main(String... mains){
 Child child = new Child();
-child.start();
+Thread thread = new Thread(child);
+thread.start();
 for(int i=0; i<10; i++)
  System.out.println("Main Thread");
 
